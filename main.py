835724e727c792bc3e623aa885cbd5d0dc0109e5
Hisham Qiblawi <hisham808@hotmail.com>
2023-11-12 03:11:31 +0200

    (2) menu and choice 1

--- main.py
+++ main.py
@@ -1,33 +1,45 @@
-import json
+def main():
+    tabs = [
+        {
+            "title": "youtube",
+            "url": "https://www.youtube.com"
+        },
+        {
+            "title": "google",
+            "url": "https://www.google.com"
+        },
+
+        {"title": "facebook",
+         "url": "https://www.facebook.com"
+         }
+    ]
+
+    def menu():
+        while True:
+            print("Hello")
+            print("1.open tab")
+            print("2.close tab")
+            print("3.switch Tab")
+            print("4.display All Tabs")
+            print("5.open Nested Tab")
+            print("6.clear All Tabs")
+            print("7.save Tabs")
+            print("8.import Tabs")
+            print("9.exit")
+            choice = int(input("Enter a number:"))
+
+            if choice == 1:
+                title = input("Enter a title: ")
+                url = input("Enter url: ")
+                open_tab(title, url)
+
+    def open_tab(title, url):
+        tabs.append({"title": title, "url": url})
+
+    print(menu())
 
 
-def tab_menu():
-    while True:
-        print("Hello")
-        print("1. open tab")
-        print("2. close tab")
-        print("3.switch Tab")
-        print("4.display All Tabs")
-        print("5.open Nested Tab")
-        print("6.clear All Tabs")
-        print("7.save Tabs")
-        print("8.import Tabs")
-        print("9.exit")
-        choice = int(input("Enter a number:"))
-
-        if choice == 1:
+if __name__ == "__main__":
+    main()
 
 
-print(tab_menu())
-
-
-def tab1(title, url):
-    return {"title": title, "url": url}
-
-
-def tab2(title, url):
-    return {"title": title, "url": url}
-
-
-def tab3(title, url):
-    return {"title": title, "url": url}
